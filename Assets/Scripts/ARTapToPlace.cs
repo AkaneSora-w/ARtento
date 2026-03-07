4bfb65e23647dec2d322e2f133cc7e6e8ab35b2d
Akane Sora <102911178+AkaneSora-w@users.noreply.github.com>
2026-03-07 16:58:36 +0100

    Full project

---
+++ Assets/Scripts/ARTapToPlace.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARTapToPlace : MonoBehaviour 
+{
+    public GameObject prefabToSpawn;
+
+    private ARRaycastManager raycastManager;
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if(Input.touchCount == 0) 
+            return;
+
+        Touch touch = Input.GetTouch(0);
+        if(touch.phase != TouchPhase.Began) 
+            return;
+
+        if(raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+        {
+            Pose hitPose = hits[0].pose;
+            Instantiate(prefabToSpawn, hitPose.position, hitPose.rotation);
+        }
+    }
+}
